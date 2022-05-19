7d3e54de2fa8274a73417a5d49da22dff04ce914
Tinkesh Kumar <tinkeshkumar@Tinkeshs-MacBook-Air.local>
2022-05-19 14:22:54 +0530

    Remove app & stream to test the issue

--- src/main/kotlin/org/jitsi/jibri/api/xmpp/XmppApi.kt
+++ src/main/kotlin/org/jitsi/jibri/api/xmpp/XmppApi.kt
@@ -390,7 +390,7 @@ class XmppApi(
                 val isRecording = serviceParams.appData?.isRecording
                 val app = serviceParams.appData?.app
                 val stream = serviceParams.appData?.stream
-                var fullRTMPUrl = "-f tee -map 0 "                
+                var fullRTMPUrl = "-flags +global_header -f tee -map 0 "
                 val teeCommand = createFfmpegTeeSelectCommand(streamKeys, streamUrls, app,
                     stream, isRecording, callName)
                 if (teeCommand.isNotEmpty()) {
@@ -449,22 +449,23 @@ class XmppApi(
     ): String {
         var teeCommand = ""
         streamKeys?.forEach {
-            teeCommand += "STREAM_MAPS[it.streamKey]}${it.streamValue}|"
+            teeCommand += "[select=\\'v:0,a\\':f=flv:onfail=ignore]${STREAM_MAPS[it.streamKey]}${it.streamValue}|"
         }
 
         streamUrls?.forEach {
-            teeCommand += "$it|"
+            teeCommand += "[select=\\'v:0,a\\':f=flv:onfail=ignore]$it|"
         }
 
         if (app != null && stream != null) {
-            teeCommand += "rtmp://srs-edge-service.streaming:1935" +
+            teeCommand += "[select=\\'v:0,a\\':f=flv:onfail=ignore]rtmp://srs-edge-service.streaming:1935" +
                     "?stream=$stream&app=$app|"
         }
 
         if (isRecording == true) {
             val suffix = "_${LocalDateTime.now().format(TIMESTAMP_FORMATTER)}.mp4"
             val filename = "${callName.take(MAX_FILENAME_LENGTH - suffix.length)}$suffix"
-            teeCommand += "-f mp4 /config/jibri/recording/veqhebbqgbacqzff/$filename.mp4|"
+            teeCommand += "[select=\\'v:0,a\\':f=flv:onfail=ignore] -f mp4 " +
+                    "/config/jibri/recording/veqhebbqgbacqzff/$filename.mp4|"
         }
 
         if (teeCommand.last().toString().equals("|", true)) {
